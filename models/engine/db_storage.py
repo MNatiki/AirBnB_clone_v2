f018a20b08c2fd2b68b3dcdc14b96d8c3ae1fa64
besufikad <07yilmabese@gmail.com>
2023-09-18 16:51:57 +0300

    task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
 from ..base_model import Base
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -8,10 +9,16 @@ from .classes import classes
 
 
 class DBStorage:
+    """This class manages storage of hbnb models in a MySQL database."""
     __engine = None
     __session = None
 
     def __init__(self):
+        """
+        The above function initializes a connection to a MySQL
+        database and creates a new database if it
+        doesn't already exist.
+        """
         connect = MySQLdb.connect(host="localhost", port=3306,
                                   user="hbnb_dev", passwd="hbnb_dev_pwd",
                                   charset="utf8")
@@ -31,6 +38,11 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
+        """
+        The function `all` retrieves all instances of a specified
+        class from a database and returns them
+        in a dictionary format.
+        """
         my_dict = {}
         Session = sessionmaker(bind=self.__engine)
         self.__session = Session()
@@ -49,22 +61,31 @@ class DBStorage:
         return my_dict
 
     def new(self, obj):
-        # print("passed new")
-        # print(obj)
+        """
+        The function adds an object to a session.
+        """
         self.__session.add(obj)
-        print(self.__session.new)
 
     def save(self):
-        # print("in db save")
-        # self.__session.commit()
-        pass
+        """
+        The save function commits changes to the session.
+        """
+        self.__session.commit()
 
     def delete(self, obj=None):
+        """
+        The delete function deletes an object from the session.
+        """
         if obj is None:
             return
         self.__session.delete(obj)
 
     def reload(self):
+        """
+        The `reload` function creates all the necessary tables
+        in the database and initializes a new
+        session.
+        """
         from ..amenity import Amenity
         from ..city import City
         from ..place import Place
