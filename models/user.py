61bc9d60a3f1e73e65594d699d381595155a728e
besufikad <07yilmabese@gmail.com>
2023-09-18 21:26:35 +0300

    task 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""
+This module defines a class User
+The User class defines a user with attributes such as email,
+password, first name, and last name.
+"""
 from models.base_model import BaseModel, Base
 from models.base_model import Column, String, Integer
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """
+    This class defines a user by various attributes
+    The User class defines a user with attributes
+    such as email, password, first name, and last name.
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
