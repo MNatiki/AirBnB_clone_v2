e5889c735f03399b2b0ab46146b0a3a92de37307
besufikad <07yilmabese@gmail.com>
2023-09-19 14:46:46 +0300

    task 9

--- models/review.py
+++ models/review.py
@@ -18,4 +18,4 @@ class Review(BaseModel, Base):
                       ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60),
                      ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024))
+    text = Column(String(1024), nullable=False)
