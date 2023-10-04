f6d7d4f02050d30d48e8ae24072fe0d44e04b9d0
besufikad <07yilmabese@gmail.com>
2023-10-04 17:20:24 +0000

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# a Bash script that sets up web servers for the deployment of web_static
+sudo apt update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "testing server configration" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/current  /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data
+sed -i "s-\tserver_name _;-server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sed -i "s-\tserver_name _;-server_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
+sudo service nginx restart
