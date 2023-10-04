0f5712fef24f9755e301205845a6291bf34ba146
besufikad <07yilmabese@gmail.com>
2023-10-04 20:59:06 +0300

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 sudo chmod a+w /data/web_static/releases/test/index.html
 echo "testing server configration" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current  /data/web_static/releases/test/
+sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites-available/default
 sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites-enabled/default
