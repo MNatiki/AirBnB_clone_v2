3a690ccfb06b4cf6f73f29e976cbda77a03de8a7
besufikad <07yilmabese@gmail.com>
2023-10-04 18:29:00 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-# a Bash script that sets up web servers for the deployment of web_static
+# a bash script that set up web server for web deployment
 sudo apt update
 sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
-sudo chmod a+w /data/web_static/releases/test/index.html
-echo "testing server configration" > /data/web_static/releases/test/index.html
-sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
+sudo echo "testing server configration files" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites-available/default
-sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites-enabled/default
+sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites_enabled/default
+sudo sed -i "s-\tserver_name _;-\tserver_name _;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n-" /etc/nginx/sites_available/default
 sudo service nginx restart
+
