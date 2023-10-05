53c183ab8d80bd1e068ec1a874501a27edfe02e3
root <root@Bese.localdomain>
2023-10-05 15:12:05 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,26 +43,23 @@ def do_deploy_run(archive_path):
     # Upload the archive to remote servers
     put(archive_path, "/tmp/")
     # If a folder with the same archive name exists, remvoe it
-    run('sudo rm -rf -- /data/web_static/releases/' + archive_name)
+    # run('sudo rm -rf -- /data/web_static/releases/' + archive_name)
 
-    run('sudo mkdir -p /data/web_static/releases/' + archive_name)
+    run('mkdir -p /data/web_static/releases/' + archive_name)
 
     # Extract the archive's contents to
     # /data/web_static/releases/<archive_name>
-    run('sudo tar -xzf /tmp/' + archive_path[9:] +
+    run('tar -xzf /tmp/' + archive_path[9:] +
         ' -C /data/web_static/releases/' + archive_name)
     # Move the contents of the extracted archive to its parent folder
-    run('sudo mv -f /data/web_static/releases/'
+    run('rm /tmp/' + archive_path[9:])
+    run('mv /data/web_static/releases/'
         + archive_name + '/web_static/* '
-        + '/data/web_static/releases/' + archive_name)
-    run('sudo rm -rf /data/web_static/releases/'
+         '/data/web_static/releases/' + archive_name)
+    run('rm -rf /data/web_static/releases/'
         + archive_name + '/web_static/')
-    run('sudo rm /tmp/' + archive_path[9:])
 
-    # '''
-    # Remove the symbolic link if it exists
-    run('sudo rm -f -- /data/web_static/current')
-    run('sudo ln -sf /data/web_static/releases/'
+    run('rm -rf /data/web_static/current')
+    run('ln -sf /data/web_static/releases/'
         + archive_path[9:-4] + ' /data/web_static/current')
     return True
-    # '''
