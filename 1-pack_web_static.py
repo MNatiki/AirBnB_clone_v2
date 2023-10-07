718bfdd2483b877989b43261d52e6027702389b5
MNatiki <natiart07@gmail.com>
2023-10-07 17:50:48 +0000

    tar

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,21 +10,21 @@ import os
 
 @runs_once
 def do_pack():
-    """
-    The function `do_pack()` creates a compressed archive file of the
-    """
-    now = datetime.now()
-    name = "web_static_" + str(now.year) + str(now.month) + \
-        str(now.day) + str(now.hour) + str(now.minute) + \
-        str(now.second) + ".tgz"
+	"""
+	The function `do_pack()` creates a compressed archive file of the
+	"""
+	now = datetime.now()
+	name = "web_static_" + str(now.year) + str(now.month) + \
+		str(now.day) + str(now.hour) + str(now.minute) + \
+		str(now.second) + ".tgz"
 
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+	if not os.path.exists("versions"):
+		os.makedirs("versions")
 
-    print("Packing web_static to versions/{}".format(name))
-    result = local("tar -cvzf versions/{} web_static".format(name))
+	print("Packing web_static to versions/{}".format(name))
+	result = local("tar -cvzf versions/{} web_static".format(name))
 
-    if result.succeeded:
-        return 'versions/' + name
-    else:
-        return None
+	if result.succeeded:
+		return 'versions/' + name
+	else:
+		return None
