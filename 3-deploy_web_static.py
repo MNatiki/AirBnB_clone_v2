ffdcf6467275f4824fa44e172bb7bcdfb1b367f4
MNatiki <natiart07@gmail.com>
2023-10-08 17:35:06 +0000

    tar

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
 The `deploy` function creates a compressed archive file of the
-`web_static` folder and saves it in the `versions` for deployment
 """
 from fabric.api import *
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
-env.hosts = ['52.201.220.122', '54.90.60.221']
+env.hosts = ['54.164.28.87', '100.26.164.50']
 
 
 def deploy():
     """
     The function "deploy" calls the "do_pack" function and
-    if it returns a valid path, it calls the "do_deploy"
-    function with that path and returns the result.
     """
     my_path = do_pack()
     # print(os.path.exists(my_path))
